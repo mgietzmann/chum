bd22a0626cab8593676051aa2df7f54574f20a8e
erebuseternal <marcelsanders96@gmail.com>
2024-01-22 06:22:15 -0500

    coming along...

--- intelliscope/mirrorverse/test.py
+++ intelliscope/mirrorverse/test.py
@@ -5,10 +5,53 @@ import json
 from tqdm import tqdm
 from time import time
 
-class Agent(object):
-    def __init__(self, position, min_difference, time_delta, velocity, friction, pub_queue, sub_queue):
+
+class ComputeContainer(object):
+
+    def __init__(self, agents, sub_queue, pub_queue):
         self.pub_queue = pub_queue
         self.sub_queue = sub_queue
+        self.agents = agents
+        self.read_state = None
+        self.write_state = []
+
+    def sense(self):
+        self.read_state = self.sub_queue.get()
+
+    def act(self):
+        for agent in self.agents:
+            agent.sense(self.read_state)
+            agent.act()
+
+        self.write_state = []
+        for agent in self.agents:
+            self.write_state.append(agent.signal())
+        self.read_state = None
+
+    def signal(self):
+        self.pub_queue.put(self.write_state)
+
+def job(agents, trigger_queue, sub_queue, pub_queue):
+    container = ComputeContainer(agents, sub_queue, pub_queue)
+    container.signal()
+    while True:
+        trigger = trigger_queue.get()
+        if not trigger:
+            break
+        container.sense()
+        container.act()
+        container.signal()
+    print('process finished')
+
+def do_exchange(sub_queues, pub_queues):
+    state = []
+    for pub_queue in pub_queues:
+        state.extend(pub_queue.get())
+    for sub_queue in sub_queues:
+        sub_queue.put(state)
+
+class Agent(object):
+    def __init__(self, position, min_difference, time_delta, velocity, friction):
         self.position = position
         self.min_difference = min_difference
         self.time_delta = time_delta
@@ -16,11 +59,8 @@ class Agent(object):
         self.friction = friction
         self.positions = []
 
-    def sense(self):
-        self.positions = []
-        while not self.sub_queue.empty():
-            signal = self.sub_queue.get()
-            self.positions.append(signal)
+    def sense(self, state):
+        self.positions = state
 
     def act(self):
         differences = self.position - np.array(self.positions)
@@ -40,7 +80,7 @@ class Agent(object):
         self.position += self.velocity * self.time_delta
 
     def signal(self):
-        self.pub_queue.put(self.position)
+        return self.position
 
 def divide_agents(agents, num_processes):
     divided_agents = [list() for _ in range(num_processes)]
@@ -48,66 +88,44 @@ def divide_agents(agents, num_processes):
         divided_agents[i % num_processes].append(agent)
     return divided_agents
 
-def do_exchange(pub_queues, sub_queues):
-    for input_queue in pub_queues:
-        input_ = input_queue.get()
-        for output_queue in sub_queues:
-            output_queue.put(input_)
-
-def job(agents, trigger_queue, finished_queue):
-    while True:
-        trigger = trigger_queue.get()
-        if not trigger:
-            break
-        for agent in agents:
-            agent.sense()
-            agent.act()
-        for agent in agents:
-            agent.signal()
-        finished_queue.put(1)
-    print('job done')
-
 @click.command()
-@click.option('-n', '--num-agents', default=25, type=int)
+@click.option('-n', '--num-agents', default=2000, type=int)
 @click.option('-d', '--min-difference', default=0.5, type=float)
 @click.option('-f', '--friction', default=0.05, type=float)
 @click.option('-t', '--time-delta', default=0.1, type=float)
-@click.option('-p', '--num-processes', default=8, type=int)
-@click.option('-s', '--time-steps', default=100, type=int)
+@click.option('-p', '--num-processes', default=10, type=int)
+@click.option('-s', '--time-steps', default=300, type=int)
 @click.option('-o', '--output-file', default='output.csv')
 def main(
     num_agents, min_difference, friction, time_delta, num_processes, time_steps, output_file
 ):
-    pub_queues = [Queue() for _ in range(num_agents)]
-    sub_queues = [Queue() for _ in range(num_agents)]
+    pub_queues = [Queue() for _ in range(num_processes)]
+    sub_queues = [Queue() for _ in range(num_processes)]
 
     print('initializing agents')
     agents = [
-        Agent(np.random.uniform(-1, 1), min_difference, time_delta, 0, friction, pub_queue, sub_queue)
-        for pub_queue, sub_queue in zip(pub_queues, sub_queues)
+        Agent(np.random.uniform(-1, 1), min_difference, time_delta, 0, friction)
+        for _ in range(num_agents)
     ]
-    for agent in agents:
-        agent.signal()
-    do_exchange(pub_queues, sub_queues)
 
     print('starting processes')
     divided_agents = divide_agents(agents, num_processes)
     trigger_queues = [Queue() for _ in range(num_processes)]
-    finished_queue = Queue()
     processes = [
-        Process(target=job, args=(sub_agents, trigger_queue, finished_queue))
-        for sub_agents, trigger_queue in zip(divided_agents, trigger_queues)
+        Process(target=job, args=(sub_agents, trigger_queue, sub_queue, pub_queue))
+        for sub_agents, trigger_queue, sub_queue, pub_queue in zip(divided_agents, trigger_queues, sub_queues, pub_queues)
     ]
     for process in processes:
         process.start()
+
+    print('initializing state')
+    do_exchange(sub_queues, pub_queues)
     
     print('running simulation')
     for _ in tqdm(range(time_steps)):
         for trigger_queue in trigger_queues:
             trigger_queue.put(1)
-        for _ in range(num_processes):
-            finished_queue.get()
-        do_exchange(pub_queues, sub_queues)
+        do_exchange(sub_queues, pub_queues)
 
     print('sending stop signals')
     for trigger_queue in trigger_queues:
