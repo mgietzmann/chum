9b2e46a25dd0a7184ffe640e25130f2863afee1b
erebuseternal <marcelsanders96@gmail.com>
2024-01-22 08:08:31 -0500

    draft of stream

--- intelliscope/mirrorverse/test.py
+++ intelliscope/mirrorverse/test.py
@@ -4,6 +4,7 @@ import click
 import json
 from tqdm import tqdm
 from time import time
+from collections import Counter, defaultdict
 
 
 class ComputeContainer(object):
@@ -23,13 +24,14 @@ class ComputeContainer(object):
             agent.sense(self.read_state)
             agent.act()
 
+    def signal(self):
         self.write_state = []
         for agent in self.agents:
             self.write_state.append(agent.signal())
         self.read_state = None
 
-    def signal(self):
         self.pub_queue.put(self.write_state)
+
 
 def job(agents, trigger_queue, sub_queue, pub_queue):
     container = ComputeContainer(agents, sub_queue, pub_queue)
@@ -43,44 +45,134 @@ def job(agents, trigger_queue, sub_queue, pub_queue):
         container.signal()
     print('process finished')
 
-def do_exchange(sub_queues, pub_queues):
-    state = []
-    for pub_queue in pub_queues:
-        state.extend(pub_queue.get())
-    for sub_queue in sub_queues:
-        sub_queue.put(state)
 
-class Agent(object):
-    def __init__(self, position, min_difference, time_delta, velocity, friction):
-        self.position = position
-        self.min_difference = min_difference
-        self.time_delta = time_delta
-        self.velocity = velocity
-        self.friction = friction
-        self.positions = []
+class Exchanger(object):
+    def __init__(self, sub_queues, pub_queues):
+        self.sub_queues = sub_queues
+        self.pub_queues = pub_queues
+        self.state = None
+
+    def accumulate_state(self):
+        self.state = {
+            'tags': [],
+            'positions': [],
+            'weights': [],
+            'winners': defaultdict(int)
+        }
+        for pub_queue in self.pub_queues:
+            container_states = pub_queue.get()
+            for new_state in container_states:
+                if new_state[0] == 'area':
+                    for tag, won_value in new_state[1].items():
+                        self.state['winners'][tag] += won_value
+                else:
+                    tag, position, weight = new_state[1]
+                    self.state['tags'].append(tag)
+                    self.state['positions'].append(position)
+                    self.state['weights'].append(weight)
+
+        self.state['tags'] = np.array(self.state['tags'])
+        self.state['positions'] = np.array(self.state['positions'])
+        self.state['weights'] = np.array(self.state['weights'])
+
+    def exchange(self):
+        self.accumulate_state()
+        for sub_queue in self.sub_queues:
+            sub_queue.put(self.state)
+        self.state = None
+
+
+class Area(object):
+    def __init__(self, value, centroid, unit_territory_radius, unit_value):
+        self.value = np.floor(value / unit_value) * unit_value
+        self.units = int(self.value / unit_value)
+        self.centroid = centroid
+        self.unit_territory_radius = unit_territory_radius
+        self.unit_value = unit_value
+        self.wins = {}
 
     def sense(self, state):
-        self.positions = state
-
+        self.fish_positions = state['positions']
+        self.fish_weights = state['weights']
+        self.fish_tags = state['tags']
+    
     def act(self):
-        differences = self.position - np.array(self.positions)
-        differences[differences == 0] = np.random.choice([-1, 1], differences[differences == 0].shape) * self.min_difference
-        forces = 1 / (differences ** 2) 
-        forces[forces > 1 / (self.min_difference ** 2)] = 1 / (self.min_difference ** 2)
-        forces = forces * np.sign(differences)
-        force = np.sum(forces)
-        self.velocity += force * self.time_delta
-        # now apply friction
-        friction_force = self.friction * self.velocity
-        current_sign = np.sign(self.velocity)
-        self.velocity -= friction_force * self.time_delta
-        if np.sign(self.velocity) != current_sign:
-            self.velocity = 0
+        differences = self.fish_positions - self.centroid
+        distances = np.linalg.norm(differences, axis=1)
+        territory_sizes = self.unit_territory_radius * np.sqrt(self.fish_weights)
 
-        self.position += self.velocity * self.time_delta
+        probs = distances
+        probs[probs < territory_sizes] = 0
+        probs = probs * self.fish_weights
+        probs = probs / np.sum(probs)
+
+        winners = np.random.choice(
+            self.fish_tags, size=self.units, p=probs, replace=True
+        )
+
+        self.wins = {}
+        for tag, num_wins in Counter(winners).items():
+            self.wins[tag] = num_wins * self.unit_value
 
     def signal(self):
-        return self.position
+        return 'area', self.wins
+
+
+class Fish(object):
+    def __init__(
+        self, tag, position, weight, habitat_centroids, habitat_values, 
+        unit_territory_radius, rate_limiter
+    ):
+        self.tag = tag
+        self.position = position
+        self.weight = weight
+        self.habitat_centroids = habitat_centroids
+        self.habitat_values = habitat_values
+        self.unit_territory_radius = unit_territory_radius
+        self.territory_radius = unit_territory_radius * np.sqrt(self.weight)
+        self.rate_limiter = rate_limiter
+
+    def sense(self, state):
+        self.fish_weights = np.array(state['weights'])
+        self.fish_positions = np.array(state['positions'])
+        self.fish_tags = np.array(state['tags'])
+
+    def act(self):
+        differences = self.fish_positions - self.position
+        distances = np.linalg.norm(differences, axis=1)
+        territory_sizes = self.unit_territory_radius * np.sqrt(self.fish_weights)
+        interaction_distances = self.territory_radius + territory_sizes
+
+        # filter down to observable fish
+        weights = self.fish_weights[(distances <= interaction_distances) & (self.fish_tags != self.tag)]
+        positions = self.fish_positions[(distances <= interaction_distances) & (self.fish_tags != self.tag)]
+        tags = self.fish_tags[(distances <= interaction_distances) & (self.fish_tags != self.tag)]
+        territory_sizes = self.unit_territory_radius * np.sqrt(weights)
+
+        # filter down to observable habitats
+        differences = self.habitat_centroids - self.position
+        habitat_distances = np.linalg.norm(differences, axis=1)
+        habitat_centroids = self.habitat_centroids[habitat_distances <= self.territory_radius]
+        habitat_values = self.habitat_values[habitat_distances <= self.territory_radius]
+
+        # compute expected value of each habitat
+        best_value = -float('inf')
+        best_centroid = None
+        for centroid, value in zip(habitat_centroids, habitat_values):
+            differences = positions - centroid
+            distances = np.linalg.norm(differences, axis=1)
+            sub_weights = weights[distances <= territory_sizes]
+            expected_value = self.weight / (np.sum(sub_weights) + self.weight) * value
+            if expected_value > best_value:
+                best_value = expected_value
+                best_centroid = centroid
+
+        direction = (best_centroid - self.position)
+        direction = direction / np.linalg.norm(direction)
+        self.position = self.position + direction * self.territory_radius * self.rate_limiter
+
+    def signal(self):
+        return 'fish', (self.tag, self.position, self.weight)
 
 def divide_agents(agents, num_processes):
     divided_agents = [list() for _ in range(num_processes)]
@@ -89,26 +181,54 @@ def divide_agents(agents, num_processes):
     return divided_agents
 
 @click.command()
-@click.option('-n', '--num-agents', default=2000, type=int)
-@click.option('-d', '--min-difference', default=0.5, type=float)
-@click.option('-f', '--friction', default=0.05, type=float)
-@click.option('-t', '--time-delta', default=0.1, type=float)
+@click.option('-n', '--num-agents', default=100, type=int)
 @click.option('-p', '--num-processes', default=10, type=int)
-@click.option('-s', '--time-steps', default=300, type=int)
+@click.option('-s', '--time-steps', default=100, type=int)
 @click.option('-o', '--output-file', default='output.csv')
 def main(
-    num_agents, min_difference, friction, time_delta, num_processes, time_steps, output_file
+    num_agents, num_processes, time_steps, output_file
 ):
     pub_queues = [Queue() for _ in range(num_processes)]
     sub_queues = [Queue() for _ in range(num_processes)]
 
-    print('initializing agents')
-    agents = [
-        Agent(np.random.uniform(-1, 1), min_difference, time_delta, 0, friction)
-        for _ in range(num_agents)
+    print('initializing habitats')
+    stream_bounds = [0, 1]
+    habitat_radius = 0.01
+
+    habitat_centroids = np.arange(
+        stream_bounds[0] + habitat_radius, 
+        stream_bounds[1],
+        habitat_radius * 2
+    )
+
+    habitat_values = habitat_centroids
+    habitat_centroids = np.array([
+        [x] for x in habitat_centroids
+    ])
+    unit_value = np.min(habitat_values)
+    unit_territory_radius = (habitat_radius * 2) * 3
+
+    habitats = [
+        Area(value, centroid, unit_territory_radius, unit_value)
+        for value, centroid in zip(habitat_values, habitat_centroids)
+    ]
+    print(len(habitats), 'habitats created')
+
+    print('initializing fish')
+    weight_bounds = [
+        max((habitat_radius / unit_territory_radius) ** 2, 0.25), 1
+    ]
+    rate_limiter = 0.25
+    fish = [
+        Fish(
+            tag, np.array([np.random.uniform(*stream_bounds)]), np.random.uniform(*weight_bounds), 
+            habitat_centroids, habitat_values, unit_territory_radius, rate_limiter
+        )
+        for tag in range(num_agents)
     ]
 
     print('starting processes')
+    agents = habitats + fish
     divided_agents = divide_agents(agents, num_processes)
     trigger_queues = [Queue() for _ in range(num_processes)]
     processes = [
@@ -118,14 +238,17 @@ def main(
     for process in processes:
         process.start()
 
+    print('building exchanger')
+    exchanger = Exchanger(sub_queues, pub_queues)
+
     print('initializing state')
-    do_exchange(sub_queues, pub_queues)
+    exchanger.exchange()
     
     print('running simulation')
     for _ in tqdm(range(time_steps)):
         for trigger_queue in trigger_queues:
             trigger_queue.put(1)
-        do_exchange(sub_queues, pub_queues)
+        exchanger.exchange()
 
     print('sending stop signals')
     for trigger_queue in trigger_queues:
@@ -142,7 +265,7 @@ def main(
                 queue.get()
 
     print('writing results')
-    results = [agent.position for agent in agents]
+    results = [list(agent.position) for agent in fish]
     with open(output_file, 'w') as f:
         json.dump(results, f)
     print('done')
